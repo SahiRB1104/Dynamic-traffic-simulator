e4a29bd455af1bf77498549f006a618b13d0171f
sahil bhalekar <129488267+SahiRB1104@users.noreply.github.com>
2026-04-30 18:38:30 +0530

    Add frontend/src/data/indiaCities.js

---
+++ frontend/src/data/indiaCities.js
@@ -0,0 +1,12 @@
+export const indiaCities = [
+  { name: "Mumbai", lat: 19.076, lon: 72.8777 },
+  { name: "Pune", lat: 18.5204, lon: 73.8567 },
+  { name: "Nashik", lat: 19.9975, lon: 73.7898 },
+  { name: "Aurangabad", lat: 19.8762, lon: 75.3433 },
+  { name: "Nagpur", lat: 21.1458, lon: 79.0882 },
+  { name: "Surat", lat: 21.1702, lon: 72.8311 },
+  { name: "Ahmedabad", lat: 23.0225, lon: 72.5714 },
+  { name: "Kolhapur", lat: 16.7049, lon: 74.2433 },
+  { name: "Solapur", lat: 17.6599, lon: 75.9064 },
+  { name: "Goa", lat: 15.2993, lon: 74.124 }
+];
