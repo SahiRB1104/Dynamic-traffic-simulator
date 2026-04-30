998fa944a5b093dc07838f3e9789bed5ddc6578b
sahil bhalekar <129488267+SahiRB1104@users.noreply.github.com>
2026-04-30 18:38:28 +0530

    Add backend/src/test/java/com/traffic/simulator/SimulatorApplicationTests.java

---
+++ backend/src/test/java/com/traffic/simulator/SimulatorApplicationTests.java
@@ -0,0 +1,15 @@
+package com.traffic.simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class SimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
